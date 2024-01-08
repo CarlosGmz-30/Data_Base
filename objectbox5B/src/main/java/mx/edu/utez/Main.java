09a5154472106a8c67237d9a9e5011c1a4fe4c02
CarlosGmz-30 <20223tn066@utez.edu.mx>
2024-01-08 14:48:33 -0600

    Editamos la clase main

--- objectbox5B/src/main/java/mx/edu/utez/Main.java
+++ objectbox5B/src/main/java/mx/edu/utez/Main.java
@@ -1,7 +1,29 @@
 package mx.edu.utez;
 
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
+import mx.edu.utez.model.MyObjectBox;
+import mx.edu.utez.model.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Referencia a la base de datps
+        BoxStore boxStore = MyObjectBox.builder()
+                .name("usuarios")
+                .build();
+        // Referencia a la colección o clase
+        Box<User> userBox = boxStore.boxFor(User.class);
+
+        // Inserción
+        User user = new User();
+        user.id = 0;
+        user.name = "Charlie";
+        userBox.put(user);
+
+        // Recuperación por Id
+        User recuperado = userBox.get(1);
+        System.out.println(recuperado);
+
+
     }
 }
--- objectbox5B/src/main/java/mx/edu/utez/model/User.java
+++ objectbox5B/src/main/java/mx/edu/utez/model/User.java
@@ -1,6 +1,11 @@
 package mx.edu.utez.model;
 
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+
+@Entity
 public class User {
+    @Id
     public long id;
     public String name;
 }
