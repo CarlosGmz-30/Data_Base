b177c443eee5c2bf4e04f8bf8e49c88971e4f91d
Carlos gmz <20223tn066@utez.edu.mx>
2024-01-08 23:27:32 -0600

    Agregamos el delete, put y consulta general

--- objectbox5B/src/main/java/mx/edu/utez/Main.java
+++ objectbox5B/src/main/java/mx/edu/utez/Main.java
@@ -5,6 +5,8 @@ import io.objectbox.BoxStore;
 import mx.edu.utez.model.MyObjectBox;
 import mx.edu.utez.model.User;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Referencia a la base de datps
@@ -24,6 +26,34 @@ public class Main {
         User recuperado = userBox.get(1);
         System.out.println(recuperado);
 
+        // Recuperar un usuario por ID
+        User userToUpdate = userBox.get(1);
+        if (userToUpdate != null) {
+            userToUpdate.setName("NuevoNombre");
+            userBox.put(userToUpdate);
+            System.out.println("Usuario actualizado: " + userToUpdate);
+        } else {
+            System.out.println("Usuario no encontrado.");
+        }
 
+        // Recuperar un usuario por ID
+        User userToDelete = userBox.get(1);
+        if (userToDelete != null) {
+            userBox.remove(userToDelete);
+            System.out.println("Usuario eliminado: " + userToDelete);
+        } else {
+            System.out.println("Usuario no encontrado.");
+        }
+
+        // Get General
+        // Obtener todos los usuarios de la base de datos
+        List<User> allUsers = userBox.getAll();
+
+        // Imprimir la información de todos los usuarios
+        System.out.println("Lista de todos los usuarios:");
+
+        for (User users : allUsers) {
+            System.out.println(users);
+        }
     }
 }
