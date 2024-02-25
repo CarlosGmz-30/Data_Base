a4f1fdacfaeb8a79ecd069d4f51034b3b87fbb12
Carlos gmz <20223tn066@utez.edu.mx>
2024-02-24 23:54:35 -0600

    Agregamos comandos de Node

--- ActsNodejs/Node.js
+++
@@ -1,5 +0,0 @@
-Para inicializar un proyecto de Note.js
-- Ejecuta el comando: npm init
-- Crea tu archivo principal 'app.js' o 'index.js'
-- Instalar dependencias: npm install express
-- Para ejecutar el servicio: node [nombre de archivo principal]
